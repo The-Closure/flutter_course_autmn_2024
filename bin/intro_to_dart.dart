5b0db16cc8f0fa44034161567d90496a31d5dfb0
AbdTll <abdalaziztll992010@gmail.com>
2024-09-07 09:11:23 -0700

    inrto to oop

--- bin/intro_to_dart.dart
+++ bin/intro_to_dart.dart
@@ -1,157 +1,39 @@
 void main(List<String> args) {
-  // int counter = 5;
-  // print(counter.runtimeType);
+  Person human = Person(name: "Rami", birthDate: DateTime(1990));
+  print(human.name);
+  print(human.birthDate);
+  print(human.getAge());
 
-  // String name = "Ahmad";
-  // print(name);
+  Person anoutherHumen = Person(name: "Sami", birthDate: DateTime(2000));
+  print(anoutherHumen.getAge());
 
-  // bool isFav = true;
-  // print(isFav);
+  Employee empl1 =
+      Employee(jop: 'Lower', name: "Noor", birthDate: DateTime(2000));
+  print(empl1.getAge());
 
-  // num age = 20;
-  // print(age.runtimeType);
+  Person testPerson =
+      Employee(jop: "IT", name: "Mhd", birthDate: DateTime(2020));
 
-  // num degree = 5.7;
-  // print(degree.runtimeType);
+  print((testPerson as Employee).jop);
+  print(testPerson.runtimeType);
 
-  // // What will print if init withoud declaration in var keyword
-  // var anything;
-  // anything = "";
-  // print(anything.runtimeType);
-
-  // var people = "Hello WOrld";
-  // // ! We can't change type later with var keyword
-  // // people = 50;
-
-  // dynamic street = "Hello";
-  // street = 300;
-  // print(street);
-
-  // int? temp;
-  // print(temp.runtimeType);
-
-  // Null empty;
-  // print(empty);
-
-  // // ! Bad state
-  // const page;
-  // page = 40;
-  // print(page);
-
-  // //  ? Good State
-  // final secondPage;
-  // secondPage = 60;
-  // print(secondPage);
-
-  List<String> names = ["Sara", "Reem", "Sundus"];
-  print(names[2]);
-  print(names.elementAt(1));
-
-  Map<String, dynamic> marks = {
-    "Name": "Hamed",
-    "Age": 22,
-    "Math 3": 59,
-  };
-
-  print(marks.keys.elementAt(2));
-
-  List<Map<String, dynamic>> nestedMap = [
-    {
-      "id": 1,
-      "name": "Persian",
-      "origin": "Iran",
-      "temperament": "Sweet, Gentle, Quiet",
-      "colors": [
-        "White",
-        "Black",
-        "Blue",
-        "Cream",
-      ],
-      "description":
-          "The Persian cat is a long-haired breed known for its luxurious coat and sweet personality. They are calm and affectionate cats that enjoy a relaxed indoor lifestyle.",
-      "image": "https://fakeimg.pl/500x500/cc5500"
-    }
-  ];
-  print(nestedMap[0]['colors'][2]);
-
-  Map<String, List<Map<String, dynamic>>> products = {
-    "products": [
-      {
-        "id": 1,
-        "title": "Essence Mascara Lash Princess",
-        "description":
-            "The Essence Mascara Lash Princess is a popular mascara known for its volumizing and lengthening effects. Achieve dramatic lashes with this long-lasting and cruelty-free formula.",
-        "category": "beauty",
-        "price": 9.99,
-        "discountPercentage": 7.17,
-        "rating": 4.94,
-        "stock": 5,
-        "tags": ["beauty", "mascara"],
-        "brand": "Essence",
-        "sku": "RCH45Q1A",
-        "weight": 2,
-        "dimensions": {"width": 23.17, "height": 14.43, "depth": 28.01},
-        "warrantyInformation": "1 month warranty",
-        "shippingInformation": "Ships in 1 month",
-        "availabilityStatus": "Low Stock",
-        "reviews": [
-          {
-            "rating": 2,
-            "comment": "Very unhappy with my purchase!",
-            "date": "2024-05-23T08:56:21.618Z",
-            "reviewerName": "John Doe",
-            "reviewerEmail": "john.doe@x.dummyjson.com"
-          },
-          {
-            "rating": 2,
-            "comment": "Not as described!",
-            "date": "2024-05-23T08:56:21.618Z",
-            "reviewerName": "Nolan Gonzalez",
-            "reviewerEmail": "nolan.gonzalez@x.dummyjson.com"
-          },
-          {
-            "rating": 5,
-            "comment": "Very satisfied!",
-            "date": "2024-05-23T08:56:21.618Z",
-            "reviewerName": "Scarlett Wright",
-            "reviewerEmail": "scarlett.wright@x.dummyjson.com"
-          }
-        ],
-        "returnPolicy": "30 days return policy",
-        "minimumOrderQuantity": 24,
-        "meta": {
-          "createdAt": "2024-05-23T08:56:21.618Z",
-          "updatedAt": "2024-05-23T08:56:21.618Z",
-          "barcode": "9164035109868",
-          "qrCode": "https://assets.dummyjson.com/public/qr-code.png"
-        },
-        "images": [
-          "https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/1.png"
-        ],
-        "thumbnail":
-            "https://cdn.dummyjson.com/products/images/beauty/Essence%20Mascara%20Lash%20Princess/thumbnail.png"
-      }
-    ]
-  };
-
-  print(products["products"]![0]["reviews"][0]["reviewerName"]);
-
-  int result = addTwoNumber(20, 30);
-  print(result);
-
-  // ? Required and Optional Named Parametares
-  increamentNumber(theNumber: 30, theIncreament: 3);
+  // ? Example of Simple Inhertince Types
+  num counter = 2;
+  print(counter.runtimeType);
 }
 
-int addTwoNumber(int firstNumber, int secondNumber) {
-  if (firstNumber > 10) {
-    print(firstNumber + secondNumber);
-    return 100;
-  } else {
-    return firstNumber + secondNumber;
+class Person {
+  String name;
+  DateTime birthDate;
+  Person({required this.name, required this.birthDate});
+
+  String getAge() {
+    return DateTime.now().difference(birthDate).toString();
   }
 }
 
-void increamentNumber({required int theNumber, int theIncreament = 1}) {
-  print(theNumber + theIncreament);
+class Employee extends Person {
+  String jop;
+
+  Employee({required this.jop, required super.name, required super.birthDate});
 }
