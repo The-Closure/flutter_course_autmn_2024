6dd6aa9ff546b4092eb00f74f906350081f2f245
AbdTll <abdalaziztll992010@gmail.com>
2024-10-02 08:56:46 -0700

    fix imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:auth_and_jwt/model/user_model.dart';
-import 'package:auth_and_jwt/service/auth_service.dart';
+
 import 'package:auth_and_jwt/view/login_page.dart';
-import 'package:auth_and_jwt/view/myinfo_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
